9d08269e95b682903fa070a00636344ac60d52cf
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-02 10:39:37 +0100

    Fixed indentation to fit my style.

--- zipHMM/viterbi.hpp
+++ zipHMM/viterbi.hpp
@@ -6,18 +6,18 @@
 #include <vector>
 
 namespace zipHMM {
-  
-  double viterbi(const std::vector<unsigned> &seq,
-		 const Matrix &initProbs,
-		 const Matrix &transProbs,
-		 const Matrix &emProbs,
-		 std::vector<unsigned> &viterbi_path);
 
-  double viterbi(const std::string &seq_filename,
-		 const Matrix &pi,
-		 const Matrix &A,
-		 const Matrix &B,
-		 std::vector<unsigned> &viterbi_path);
+    double viterbi(const std::vector<unsigned> &seq,
+                   const Matrix &initProbs,
+                   const Matrix &transProbs,
+                   const Matrix &emProbs,
+                   std::vector<unsigned> &viterbi_path);
+
+    double viterbi(const std::string &seq_filename,
+                   const Matrix &pi,
+                   const Matrix &A,
+                   const Matrix &B,
+                   std::vector<unsigned> &viterbi_path);
 
 }
 
