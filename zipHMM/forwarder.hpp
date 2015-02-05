66ca4663151dc6d603d814a7cde9daf69c1a315c
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-05 10:39:57 +0100

    Don't return void functions.

--- zipHMM/forwarder.hpp
+++ zipHMM/forwarder.hpp
@@ -73,51 +73,51 @@ namespace zipHMM {
         };
 
         void write_to_directory(const std::string &directory) const {
-            return ds.write_to_directory(directory);
+            ds.write_to_directory(directory);
         };
 
         void init_data_structure(const std::vector<std::vector<unsigned> > &sequences,
                                  const size_t alphabet_size, std::vector<size_t> &nStatesSave,
                                  const size_t min_no_eval) {
-            return ds.init_data_structure(sequences, alphabet_size, nStatesSave, min_no_eval);
+            ds.init_data_structure(sequences, alphabet_size, nStatesSave, min_no_eval);
         };
 
         void read_seq(const std::string &seq_filename, const size_t alphabet_size,
                       const size_t min_no_eval = 1) {
-            return ds.read_seq(seq_filename, alphabet_size, min_no_eval);
+            ds.read_seq(seq_filename, alphabet_size, min_no_eval);
         };
 
         void read_seq(const std::string &seq_filename, const size_t alphabet_size,
                       std::vector<size_t> &nStatesSave, const size_t min_no_eval = 1) {
-            return ds.read_seq(seq_filename, alphabet_size, nStatesSave, min_no_eval);
+            ds.read_seq(seq_filename, alphabet_size, nStatesSave, min_no_eval);
         };
 
         void read_seq(const std::string &seq_filename, const size_t alphabet_size,
                       const size_t no_states, const size_t min_no_eval) {
-            return ds.read_seq(seq_filename, alphabet_size, no_states, min_no_eval);
+            ds.read_seq(seq_filename, alphabet_size, no_states, min_no_eval);
         }; // convenient
 
         void read_seq_directory(const std::string &dir_filename, const size_t alphabet_size,
                                 const size_t min_no_eval = 1) {
-            return ds.read_seq_directory(dir_filename, alphabet_size, min_no_eval);
+            ds.read_seq_directory(dir_filename, alphabet_size, min_no_eval);
         };
 
         void read_seq_directory(const std::string &dir_filename, const size_t alphabet_size,
                                 std::vector<size_t> &nStatesSave, const size_t min_no_eval = 1) {
-            return ds.read_seq_directory(dir_filename, alphabet_size, nStatesSave, min_no_eval);
+            ds.read_seq_directory(dir_filename, alphabet_size, nStatesSave, min_no_eval);
         };
 
         void read_seq_directory(const std::string &dir_filename, const size_t alphabet_size,
                                 const size_t no_states, const size_t min_no_eval) {
-            return ds.read_seq_directory(dir_filename, alphabet_size, no_states, min_no_eval);
+            ds.read_seq_directory(dir_filename, alphabet_size, no_states, min_no_eval);
         };
 
         void read_from_directory(const std::string &dirname) {
-            return ds.read_from_directory(dirname);
+            ds.read_from_directory(dirname);
         };
 
         void read_from_directory(const std::string &directory, const size_t no_states) {
-            return ds.read_from_directory(directory, no_states);
+            ds.read_from_directory(directory, no_states);
         };
 
     private:
