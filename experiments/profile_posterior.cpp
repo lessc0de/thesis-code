e6edcbef5b06d2929d3958364867c1031b291f16
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-24 17:41:34 +0100

    Program for profiling posterior decoding.

---
+++ experiments/profile_posterior.cpp
@@ -0,0 +1,66 @@
+//=========================================================================
+// This file is part of HMMlib.
+//
+// HMMlib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// HMMlib is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with HMMlib. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2010  Bioinformatics Research Centre, Aarhus University.
+// Author: Andreas Sand (asand@birc.au.dk)
+//=========================================================================
+
+#include "../zipHMM/viterbi.hpp"
+#include "../zipHMM/hmm_io.hpp"
+#include "../fasta/fasta_reader.hpp"
+#include "../zipHMM/forwarder.hpp"
+#include "../zipHMM/timer.hpp"
+#include "../zipHMM/seq_io.hpp"
+#include "../zipHMM/hmm_suite.hpp"
+
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+
+#ifdef WITH_OMP
+#include<omp.h>
+#endif
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout << "Please provide HMM model and fasta file." << std::endl;
+        exit(1);
+    }
+
+    // Read HMM.
+    zipHMM::Matrix init_probs;
+    zipHMM::Matrix trans_probs;
+    zipHMM::Matrix em_probs;
+    zipHMM::read_HMM(init_probs, trans_probs, em_probs, std::string(argv[1]));
+
+    std::string sequence_filename = argv[2];
+
+    std::vector<unsigned> my_viterbi_path;
+    zipHMM::HMMSuite h;
+    size_t alphabet_size = 4;
+    size_t min_num_of_evals = 0;
+
+    h.read_seq(sequence_filename, alphabet_size, min_num_of_evals);
+
+    for (int i = 0; i < 50; ++i) {
+        h.posterior_decoding(init_probs, trans_probs, em_probs, my_viterbi_path);
+    }
+
+    exit(0);
+}
