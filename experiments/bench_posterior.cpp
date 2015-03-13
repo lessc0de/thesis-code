366e8b251d87b74eba4e1f8bd81528b69c5a405c
Torben Muldvang Andersen <muldvang@gmail.com>
2015-03-13 13:29:15 +0100

    More general posterior experiment.

--- experiments/bench_posterior.cpp
+++ experiments/bench_posterior.cpp
@@ -19,37 +19,39 @@
 // Author: Andreas Sand (asand@birc.au.dk)
 //=========================================================================
 
-#include "../zipHMM/viterbi.hpp"
 #include "../zipHMM/hmm_io.hpp"
-#include "../fasta/fasta_reader.hpp"
 #include "../zipHMM/timer.hpp"
 #include "../zipHMM/seq_io.hpp"
-#include "../zipHMM/posterior_decoding.hpp"
-#include "../zipHMM/matrix.hpp"
 #include "../zipHMM/hmm_suite.hpp"
+#include "../zipHMM/posterior_decoding.hpp"
 
 #include <string>
 #include <iomanip>
 #include <vector>
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
+#include <fstream>
 
 #ifdef WITH_OMP
 #include<omp.h>
+
 #endif
 
 int main(int argc, char **argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: model sequence n." << std::endl;
+    if (argc != 6) {
+        std::cerr << "Usage: sequence T model N filename" << std::endl;
         exit(1);
     }
 
-    std::string hmm_filename = argv[1];
+    std::string sequence_filename = argv[1];
+    int T = atoi(argv[2]);
+    std::string hmm_filename = argv[3];
+    int N = atof(argv[4]);
+    std::ofstream output(argv[5], std::ios::out | std::ios::app);
 
-    std::string seq_filename = argv[2];
-
-    int n = atof(argv[3]);
+    if (!output.is_open()) {
+        std::cout << "Unable to open output file." << std::endl;
+    }
 
     // Read HMM.
     zipHMM::Matrix pi;
@@ -63,17 +65,12 @@ int main(int argc, char **argv) {
 
     // Simple
     {
-        std::cout << n << " ";
-
         simple_timer.start();
-
         zipHMM::Matrix pd_table;
         std::vector<double> scales;
         std::vector<unsigned> pd_path;
-        zipHMM::posterior_decoding(seq_filename, pi, A, B, pd_path, pd_table);
-
+        zipHMM::posterior_decoding(sequence_filename, pi, A, B, pd_path, pd_table);
         simple_timer.stop();
-        std::cout << simple_timer.timeElapsed() << " ";
     }
 
     // zipHMMlib
@@ -83,14 +80,18 @@ int main(int argc, char **argv) {
         zipHMM::HMMSuite f;
         int alphabet_size = 4;
         int min_num_of_evals = 0;
-        f.read_seq(seq_filename, alphabet_size, min_num_of_evals);
+        f.read_seq(sequence_filename, alphabet_size, min_num_of_evals);
 
         std::vector<unsigned> pd_path;
         f.posterior_decoding(pi, A, B, pd_path);
 
         zipHMMlib_timer.stop();
-        std::cout << zipHMMlib_timer.timeElapsed() << std::endl;
     }
 
+    output << N << " "
+           << T << " "
+           << simple_timer.timeElapsed() << " "
+           << zipHMMlib_timer.timeElapsed() << std::endl;
+    output.close();
     exit(0);
 }
