e80c45810a9753210cdfe6065bc3671bc469b238
Torben Muldvang Andersen <muldvang@gmail.com>
2015-06-04 11:14:42 +0200

    Simplified classical algorithm.

--- zipHMM/viterbi.cpp
+++ zipHMM/viterbi.cpp
@@ -45,86 +45,62 @@ namespace zipHMM {
             }
         }
         for (size_t i = 0; i < no_states; ++i) {
-            for (size_t c = 0; c < 4; ++c) {
+            for (size_t c = 0; c < B.get_width(); ++c) {
                 logB(i, c) = std::log(logB(i, c));
             }
         }
+
+        // Set up matrices for computation.
         Matrix viterbi_table;
-        Matrix tmp;
+        Matrix tmp(no_states, 1);
+        Matrix res(no_states, 1);
         if (compute_path) {
             viterbi_table.reset(no_states, length);
-        } else {
-            viterbi_table.reset(no_states, 1);
-            tmp.reset(no_states, 1);
         }
 
-        //init
+        // Initial state.
         for(size_t r = 0; r < no_states; ++r) {
             viterbi_table(r, 0) = logpi(r, 0) + logB(r, seq[0]);
+            res(r, 0) = logpi(r, 0) + logB(r, seq[0]);
         }
 
-        // recursion
+        // Recursion.
         for(size_t c = 1; c < length; ++c) {
             for(size_t r = 0; r < no_states; ++r) {
                 double max_value = -std::numeric_limits<double>::max();
+                size_t max_state = 0;
+                for(size_t prev = 0; prev < no_states; ++prev) {
+                    double value = res(prev, 0) + logA(prev, r) + logB(r, seq[c]);
+                    if (value > max_value) {
+                        max_value = value;
+                        max_state = prev;
+                    }
+                }
+                tmp(r,0) = max_value;
                 if (compute_path) {
-                    for(size_t prev_state = 0; prev_state < no_states; ++prev_state) {
-                        max_value = std::max(max_value, viterbi_table(prev_state, c - 1) + logA(prev_state, r) + logB(r, seq[c]));
-                    }
-                    viterbi_table(r,c) = max_value;
-                } else {
-                    for(size_t prev_state = 0; prev_state < no_states; ++prev_state) {
-                        max_value = std::max(max_value, viterbi_table(prev_state, 0) + logA(prev_state, r) + logB(r, seq[c]));
-                    }
-                    tmp(r, 0) = max_value;
+                    viterbi_table(r, c) = max_state;
                 }
             }
+            Matrix::copy(tmp, res);
+        }
 
-            if (!compute_path) {
-                Matrix::copy(tmp, viterbi_table);
+        // Log-likelihood and end state.
+        double path_ll = res(0, 0);
+        size_t end_state = 0;
+        for(size_t r = 1; r < no_states; ++r) {
+            if(res(r, 0) > path_ll) {
+                path_ll = res(r, 0);
+                end_state = r;
             }
         }
 
-        double path_ll;
-        size_t end_point;
-        if (compute_path) {
-            path_ll = viterbi_table(0, length - 1);
-            end_point = 0;
-            for(size_t r = 1; r < no_states; ++r) {
-                if(viterbi_table(r, length - 1) > path_ll) {
-                    path_ll = viterbi_table(r, length - 1);
-                    end_point = r;
-                }
-            }
-        } else {
-            path_ll = viterbi_table(0, 0);
-            end_point = 0;
-            for(size_t r = 1; r < no_states; ++r) {
-                if(viterbi_table(r, 0) > path_ll) {
-                    path_ll = viterbi_table(r, 0);
-                    end_point = r;
-                }
-            }
-        }
-
-        // backtrack
+        // Backtracking.
         if (compute_path) {
             viterbi_path.resize(length);
-            viterbi_path[length - 1] = unsigned(end_point);
+            viterbi_path[length - 1] = end_state;
 
-            size_t current_state = end_point;
-            for(size_t c = length - 1; c > 0; --c) {
-                double max_value = viterbi_table(0, c - 1) + logA(0, current_state) + logB(current_state, seq[c]);
-                size_t max_state = 0;
-                for(size_t prev_state = 1; prev_state < no_states; ++prev_state) {
-                    double prev_state_ll = viterbi_table(prev_state, c - 1) + logA(prev_state, current_state) + logB(current_state, seq[c]);
-                    if(prev_state_ll > max_value) {
-                        max_value = prev_state_ll;
-                        max_state = prev_state;
-                    }
-                }
-                viterbi_path[c - 1] = unsigned(max_state);
-                current_state = max_state;
+            for (size_t c = length - 1; c > 0; --c) {
+                viterbi_path[c - 1] = viterbi_table(viterbi_path[c], c);
             }
         }
         return path_ll;
